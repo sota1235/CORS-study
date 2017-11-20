a2e62f07c7bcecb4f168f56efbe2922df6b8c4de
sota1235 <sota1235@gmail.com>
2017-11-21 02:09:08 +0900

    modify: hosting static javascript

--- hosting.js
+++ hosting.js
@@ -1,10 +1,26 @@
 const http = require('http');
 const pug = require('pug');
 const path = require('path');
+const fs = require('fs');
 
 const PORT = 9000;
+const ASSETS_PATH = path.join(__dirname, 'assets');
 
 const server = http.createServer((req, res) => {
+  const lookup = decodeURI(req.url);
+
+  if (lookup === '/assets/index.js') {
+    res.writeHead(200, { 'Content-Type': 'application/javascript; charset=utf-8' });
+
+    fs.readFile(path.join(ASSETS_PATH, 'index.js'), 'utf8', (err, text) => {
+      if (err) {
+        res.end('error');
+      }
+      res.end(text);
+    });
+    return;
+  }
+
   const html = pug.renderFile(path.join(__dirname, 'template', 'index.pug'));
   res.end(html);
 });
