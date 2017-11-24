4d0f0eea98720fad78a5af7047b9f5336f7416d2
sota1235 <sota1235@gmail.com>
2017-11-24 20:00:21 +0900

    modify: add sample for preflight

---
+++ cors_with_preflight/assets/index.js
@@ -0,0 +1,24 @@
+const apiServer = 'http://localhost:8000';
+
+// Fetch API
+const headers = new Headers();
+// headers.append('X-ORIGINAL', 'hoge');
+
+fetch(apiServer, {
+  method: 'PUT',
+  headers,
+  credentials: 'include',
+})
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    // Do nothing
+  });
+
+// XHLHttpRequest
+const xhr = new XMLHttpRequest();
+xhr.open('GET', apiServer, true);
+xhr.withCredentials = true;
+xhr.onload = (res) => { console.log(res); };
+// xhr.send();
